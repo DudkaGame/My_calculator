98bf30b5a435dccea762d9c86caef49eca8221bc
DudkaGame <dimaponomar2019@gmail.com>
2023-03-30 12:52:52 +0300

    Попробовал сделать расчеты

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "cmath"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +12,37 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+double directionAngle, increment, x_1, y_1, x_2, y_2 = 0;
+int scaleFactor, quarter = 0;
+bool mode = 1;
+
+
+double calculation(int mode){
+
+
+    switch (mode) {
+
+    case true:
+        x_2 = (cos(directionAngle * M_PI / 180) * increment + x_1) * scaleFactor * quarter;
+        y_2 = (sin(directionAngle * M_PI / 180) * increment + x_1) * scaleFactor * quarter;
+        return(x_2);
+    break;
+
+    case false:
+
+    break;
+
+    }
+
+}
+
+void MainWindow::on_сalculateButton_clicked(){
+
+
+
+    ui->resultLabel->setText(QString("Координаты второй точки: %1").arg(calculation(mode)));
+
 }
 
