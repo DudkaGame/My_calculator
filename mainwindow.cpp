0743922babcea84e1ee3ea2dbd7dd97af3ff6f82
DudkaGame <dimaponomar2019@gmail.com>
2023-04-01 16:43:33 +0300

    формула, учитывающие четверти

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,10 @@
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
-  QIntValidator* validator = new QIntValidator(0, 10000, this);  // Ограничить ввод от 0 до 100
-  ui->x_1->setValidator(validator);
-  ui->y_1->setValidator(validator);
+
+  //  QIntValidator* validator = new QIntValidator(0, 10000, this);  // Ограничить ввод от 0 до 100
+  //  ui->x_1->setValidator(validator);
+  //  ui->y_1->setValidator(validator);
 }
 
 MainWindow::~MainWindow()
@@ -16,7 +17,6 @@ MainWindow::~MainWindow()
   delete ui;
 }
 
-double directionAngle, increment, x_1, y_1, x_2, y_2 = 0;
 int scaleFactor, quarter = 0;
 bool mode = 1;
 bool error = 0;
@@ -26,6 +26,23 @@ bool error = 0;
 void MainWindow::on_Calculate_button_clicked()
 {
   QString strX, strY, strDirectionAngle, strIncrement, strScale = "";
+  double x_2, y_2 = 0;
+  int quarterX, quarterY;
+
+  strX = ui->x_1->text();
+  double x_1 = strX.toDouble();
+
+  strY = ui->y_1->text();
+  double y_1 = strY.toDouble();
+
+  strDirectionAngle = ui->directionAngle->text();
+  double directionAngle = strDirectionAngle.toDouble();
+
+  strIncrement = ui->increment->text();
+  double increment = strIncrement.toDouble();
+
+  strScale = ui->scale->text();
+  double scale = strScale.toDouble();
 
   if (error)
   {
@@ -37,10 +54,33 @@ void MainWindow::on_Calculate_button_clicked()
   {
     if (mode)
     {
-      x_2 = (cos(directionAngle * M_PI / 180) * increment + x_1);
-      y_2 = (sin(directionAngle * M_PI / 180) * increment + y_1);
+      int quarterInt = trunc(directionAngle / 90);
+      switch (quarterInt)
+      {
+        case 1:
+          quarterX = 1;
+          quarterY = 1;
+          break;
+        case 2:
+          quarterX = -1;
+          quarterY = 1;
+          directionAngle = 180 - directionAngle;
+          break;
+        case 3:
+          quarterX = -1;
+          quarterY = -1;
+          directionAngle = 270 - directionAngle;
+          break;
+        case 4:
+          quarterX = 1;
+          quarterY = -1;
+          directionAngle = 360 - directionAngle;
+          break;
+      }
+      x_2 = ((cos(directionAngle * M_PI / 180) * (increment / scale)) + (quarterX * x_1));
+      y_2 = ((sin(directionAngle * M_PI / 180) * (increment / scale)) + (quarterY * y_1));
 
-      ui->resultLabel->setText(QString("Координаты второй точки: x2: %1  y2:").arg(x_2, y_2));
+      ui->resultLabel->setText(QString("Координаты второй точки: x2: %1  y2: %2").arg(x_2).arg(y_2));
     }
   }
 }
